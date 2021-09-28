cb8096b350491b7c1e9c499a7cd7b53bf6040a7b
Carlos <carlosparedesrouyet@gmail.com>
2021-09-28 16:08:51 +0200

    Update build.sh

--- jenkins/deploy/build.sh
+++ jenkins/deploy/build.sh
@@ -4,6 +4,8 @@
 
 cp -f java-app/target/*.jar jenkins/deploy/
 
+cd jenkins/deploy && docker-compose down
+
 echo "######################"
 echo "*** Building image ***"
 echo "######################"
