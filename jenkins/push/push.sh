c18c82065f7b756503f343e38f3f14af18d672aa
Carlos <carlosparedesrouyet@gmail.com>
2021-09-28 13:24:38 +0200

    app

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -8,7 +8,7 @@ REGISTRY="192.168.117.128:5000"
 IMAGE="app"
 
 echo "*** Logging in ***"
-docker login -u user1 -p $PASS $REGISTRY
+#docker login -u user1 -p $PASS $REGISTRY
 echo "*** Tagging image ***"
 docker tag $IMAGE:$BUILD_TAG $REGISTRY/$IMAGE:$BUILD_TAG
 echo "*** Pushing image ***"
